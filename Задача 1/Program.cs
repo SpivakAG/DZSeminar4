f8e6f697641f26cb83d973376474b6f7d22407f4
ArtemSpivak <spivak25@yandex.ru>
2022-04-21 22:40:43 +0300

    задача решена

--- Задача 1/Program.cs
+++ Задача 1/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int number = Input("Введите число, которое нужно возвести в степень");
+int powerOfNumber = Input("Введите степень числа");
+int result = number;
+
+if (powerOfNumber>1)
+{
+        for (int i = 1; i < powerOfNumber; i++)
+    {
+        result = number * result;
+    }
+    Console.WriteLine(result);
+
+}
+else Console.WriteLine($"Введенная степень {powerOfNumber} не является натуральным числом");
+
+int Input(string text)
+    {
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+    }
